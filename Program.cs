d150f5f8d476b5bee6b0c8a83ffd8b167bcbc136
Mikael Larsson <s.mikael.larsson@gmail.com>
2025-05-03 15:19:34 +0200

    fixat transparent header och problem med padding på kartsidan

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-//service
+//service, singleton för att datan rutten ska vara kvar när användaren går från kartan
 builder.Services.AddSingleton<TripService>();
 
 //inkludera mubblazor
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,38 +1,57 @@
 let mapInstance;
 
 window.initMap = async function () {
-    const { Map } = await google.maps.importLibrary("maps");
+    try {
+        const { Map } = await google.maps.importLibrary("maps");
 
-    mapInstance = new Map(document.getElementById("map"), {
-        center: { lat: 59.3293, lng: 18.0686 },
-        zoom: 7,
-        mapId: "DEMO_MAP_ID"
-    });
+        mapInstance = new Map(document.getElementById("map"), {
+            //fallback centrering om rutten inte fungerar
+            center: { lat: 59.3293, lng: 18.0686 },
+            zoom: 7,
+            mapId: "DEMO_MAP_ID"
+        });
 
-    window.mapInstance = mapInstance;
+        window.mapInstance = mapInstance;
+    } catch (error) {
+        console.error("Kunde inte ladda Google Maps:", error);
+    }
+
 };
 
 window.drawTrip = async function (polyline, places) {
     if (!window.mapInstance || !polyline || !places) return;
 
     const { encoding } = await google.maps.importLibrary("geometry");
+    const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
 
     const path = encoding.decodePath(polyline);
+
+    //centrera kartan på rutten
+    const bounds = new google.maps.LatLngBounds();
+    path.forEach(coord => bounds.extend(coord));
+    window.mapInstance.fitBounds(bounds);
 
     const route = new google.maps.Polyline({
         path: path,
         geodesic: true,
-        strokeColor: "#FF0000",
+        strokeColor: "#FFDF78",
         strokeOpacity: 1.0,
-        strokeWeight: 4
+        strokeWeight: 2
     });
     route.setMap(window.mapInstance);
 
     for (const place of places) {
-        new google.maps.Marker({
-            position: { lat: place.lat, lng: place.lng },
-            map: window.mapInstance,
-            title: place.name
+        const pin = new PinElement({
+            background: "#009688",
+            borderColor: "#223843",
+            glyphColor: "#FFD54F",
         });
+
+        new AdvancedMarkerElement({
+            map: window.mapInstance,
+            position: { lat: place.lat, lng: place.lng },
+            content: pin.element,
+            title: place.name
+        })
     }
 };
